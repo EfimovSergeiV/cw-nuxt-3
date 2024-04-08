231ffa27603372ba0d64f8da2b0fbcc8331f62be
anon <efimovsergeiv@gmail.com>
2024-04-08 08:35:02 +0300

    add:

--- components/BreadCrumbs.vue
+++ components/BreadCrumbs.vue
@@ -1,4 +1,5 @@
 <script setup>
+  const route = useRoute()
   const props = defineProps(['breadcrumbs'])
 
 </script>
@@ -25,7 +26,7 @@
           <div class="flex items-center">
             <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
             
-            <p v-if="pk === props.breadcrumbs.length - 1" class="ml-1 text-xs text-center font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-300 dark:hover:text-gray-100">{{ path.name }}</p>
+            <p v-if="pk === props.breadcrumbs.length - 1 && route.name !== 'product-id'" class="ml-1 text-xs text-center font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-300 dark:hover:text-gray-100">{{ path.name }}</p>
             <nuxt-link v-else :to="{ name: 'prods', query: { ct: path.id, page: 1 }}" class="ml-1 text-xs text-center font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-300 dark:hover:text-gray-100">{{ path.name }}</nuxt-link>
 
           </div>
