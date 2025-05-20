7c5b25fbb7097caf3e401028939390cd2e831620
S.Efimov <sys@tehnosvar.ru>
2025-05-20 14:42:17 +0300

    Add:

--- components/CartForm.vue
+++ components/CartForm.vue
@@ -221,6 +221,85 @@
     }
   }
 
+
+  /// THIS IS TEST
+  import { onMounted } from 'vue'
+
+  onMounted(() => {
+    const phoneInputs = document.querySelectorAll('input[data-tel-input]')
+
+    const getInputNumbersValue = (input) => {
+      return input.value.replace(/\D/g, '')
+    }
+
+    const onPhonePaste = (e) => {
+      const input = e.target
+      const inputNumbersValue = getInputNumbersValue(input)
+      const pasted = e.clipboardData || window.clipboardData
+      if (pasted) {
+        const pastedText = pasted.getData('Text')
+        if (/\D/g.test(pastedText)) {
+          input.value = inputNumbersValue
+        }
+      }
+    }
+
+    const onPhoneInput = (e) => {
+      const input = e.target
+      let inputNumbersValue = getInputNumbersValue(input)
+      const selectionStart = input.selectionStart
+      let formattedInputValue = ""
+
+      if (!inputNumbersValue) {
+        input.value = ""
+        return
+      }
+
+      if (input.value.length !== selectionStart) {
+        if (e.data && /\D/g.test(e.data)) {
+          input.value = inputNumbersValue
+        }
+        return
+      }
+
+      if (["7", "8", "9"].includes(inputNumbersValue[0])) {
+        if (inputNumbersValue[0] === "9") inputNumbersValue = "7" + inputNumbersValue
+        const firstSymbols = (inputNumbersValue[0] === "8") ? "8" : "+7"
+        formattedInputValue = firstSymbols + " "
+        if (inputNumbersValue.length > 1) {
+          formattedInputValue += '(' + inputNumbersValue.substring(1, 4)
+        }
+        if (inputNumbersValue.length >= 5) {
+          formattedInputValue += ') ' + inputNumbersValue.substring(4, 7)
+        }
+        if (inputNumbersValue.length >= 8) {
+          formattedInputValue += '-' + inputNumbersValue.substring(7, 9)
+        }
+        if (inputNumbersValue.length >= 10) {
+          formattedInputValue += '-' + inputNumbersValue.substring(9, 11)
+        }
+      } else {
+        formattedInputValue = '+' + inputNumbersValue.substring(0, 16)
+      }
+
+      input.value = formattedInputValue
+    }
+
+    const onPhoneKeyDown = (e) => {
+      const inputValue = e.target.value.replace(/\D/g, '')
+      if (e.key === 'Backspace' && inputValue.length === 1) {
+        e.target.value = ""
+      }
+    }
+
+    phoneInputs.forEach(input => {
+      input.addEventListener('keydown', onPhoneKeyDown)
+      input.addEventListener('input', onPhoneInput)
+      input.addEventListener('paste', onPhonePaste)
+    })
+  })
+
+
 </script>
 
 
@@ -511,9 +590,10 @@
                   <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                     <p class="mdi mdi-phone"></p>
                   </div>
-                  <input v-model="clientStore.client.phone" type="text" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-700 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="+7 (987) 654 32 10">
+                  <input v-model="clientStore.client.phone" data-tel-input type="text" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-700 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="+7 (___) ___-__-__">
                 </div> 
               </div>
+
 
               <div class="text-xs text-gray-700 dark:text-gray-300 pb-4 border border-black/10 dark:border-white/10 p-4 rounded-md">
                 <div class="grid grid-cols-1 gap-3">
